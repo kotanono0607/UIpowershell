e945aa11b0621a696c18b40fa52c5e3dca3e7b82
kotanono0607 <halloween0129@gmai.com>
2025-10-30 22:57:50 +0900

    自動一時コミット(auto_merge_claude_into_main.bat)

--- 個々の履歴/TA/output.ps1
+++ 個々の履歴/TA/output.ps1
@@ -1,7 +1,20 @@
-﻿for ($i = 0; $i -lt 1; $i += 1) {
+﻿
+
+for ($i = 0; $i -lt 1; $i += 1) {
 Write-Host "OK"
 Write-Host "OK"
 }
 
+
+if ("2" -eq "1") {
+Write-Host "OK"
+} else {
+Write-Host "OK"
+}
+
+
+for ($i = 1; $i -lt 2; $i += 1) {
+
+}
 
 
