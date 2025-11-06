c1cdd33a71230be627867cb6d81db1eb2724e1ee
kotanono0607 <halloween0129@gmai.com>
2025-11-07 07:14:52 +0900

    Update: 更新 (7 ファイル)

--- 00_code/1-6.ps1
+++ 00_code/1-6.ps1
@@ -1,6 +1,6 @@
 ﻿function 1_6 {
 @"
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("これはメッセージボックスです。", "タイトル", "OK", "Information")
+[System.Windows.Forms.MessageBox]::Show("これはメッセージボックスです!!。", "タイトル", "OK", "Information")
 "@
 }
